2e575bd12a5dc12791af9db24efda48ad3fe73b1
cornellreddick <cornellreddick@outlook.com>
2022-01-05 15:21:09 -0500

    Created a custom event to bind two way to add different servers
    Signed-off-by: cornellreddick <cornellreddick@outlook.com>

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-cockpit',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   newServerName = '';
   newServerContent = '';
 
@@ -13,5 +16,18 @@ export class CockpitComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  onAddServer(){
+    this.serverCreated.emit({
+      serverName: this.newServerName, 
+      serverContent: this.newServerContent});
+  }
+
+  onAddBlueprint(){
+    this.blueprintCreated.emit({
+      serverName: this.newServerName, 
+      serverContent: this.newServerContent});
+  }
+
 
 }
